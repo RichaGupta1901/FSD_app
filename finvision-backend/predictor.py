13da6111fd9d77d0c2c66afc5484c5cac490153a
Richa Gupta <114852296+RichaGupta1901@users.noreply.github.com>
2025-04-23 07:11:12 +0530

    Update predictor.py

--- finvision-backend/predictor.py
+++ finvision-backend/predictor.py
@@ -6,7 +6,7 @@ import numpy as np
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True, origins=["http://localhost:3000"])
+CORS(app, supports_credentials=True, origins=['http://localhost:3000', 'https://fsd-app-frontend.onrender.com'])
 
 models = {}
 scalers = {}
